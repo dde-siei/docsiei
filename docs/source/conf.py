f0f5e71adb4d6ff6c648d5a606aecca491a7f94d
dde-siei <herramientasdeinformacion@sernanp.gob.pe>
2025-12-17 13:35:49 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,7 @@ extensions = ['sphinx_rtd_theme']
 templates_path = ['_templates']
 exclude_patterns = []
 
-language = 'en'
+language = 'es'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
